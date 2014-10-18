b193c91241a75f7519e64f68a9799a36984d6c02
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-10-18 15:42:41 +0300

    Enabled WebGL on iOS 8 Changed background color to black

--- src/game/config.js
+++ src/game/config.js
@@ -5,12 +5,18 @@ pandaConfig = {
     system: {
         width: 768,
         height: 1024,
-        rotateScreen: false
+        rotateScreen: false,
+        bgColor: '#000000'
     },
     analytics: {
         id: 'UA-42024756-5'
     },
     storage: {
         id: 'net.pandajs.flyingdog'
+    },
+    iOS8: {
+        system: {
+            webGL: true
+        }
     }
 };
